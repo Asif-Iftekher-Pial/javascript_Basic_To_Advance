2b32cfd5a939e7bbffbc26118d45ddf2d8b73f1f
Asif Iftekher Pial <iftekherpial67@gmail.com>
2022-01-20 17:07:33 +0600

    Javascript learning-Day-1

---
+++ lecture_5 toFixed_toPrecision/lecture_5.js
@@ -0,0 +1,7 @@
+var num=10;
+console.log(typeof(num));
+ var string="10";
+ console.log(typeof(string));
+ var numbers=5.15465;
+ console.log((numbers.toFixed(2))); //taking number after decimal
+ console.log(Number("10"));//converting tring to number format
